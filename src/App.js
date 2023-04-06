688afe114703002ffb5c79dd81a973545ef83f5e
lamngoac <hoangnampro2000@gmail.com>
2023-04-06 14:20:53 +0700

    first base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">Test font</header>
+            <p>
+                For test <b>the</b> true
+            </p>
+        </div>
+    );
+}
+
+export default App;
